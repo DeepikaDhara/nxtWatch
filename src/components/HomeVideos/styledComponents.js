4deea24ab42496fcf08ad0ab896803d3180ba2e3
Deepika Dhara <dharadeepika18@gmail.com>
2024-08-31 13:47:33 +0000

    nxtWatchApp1

--- src/components/HomeVideos/styledComponents.js
+++ src/components/HomeVideos/styledComponents.js
@@ -1,30 +1,31 @@
 import styled from 'styled-components'
 
-export const FailedView = styled.div`
+export const NoVideosView = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   background: none;
 `
-export const FailedImage = styled.img`
+
+export const NoVideosImage = styled.img`
   width: 200px;
   @media screen and (min-width: 768px) {
     width: 450px;
-  } ;
+  }
 `
-export const FailedHeading = styled.h1`
-  font-family: 'Roboto';
+export const NoVideosHeading = styled.h1`
+  font-family: Roboto;
   font-size: 25px;
   color: ${props => props.headingColor};
-  text-align: center;
 `
-export const FailedNote = styled.p`
-  font-family: 'Roboto';
+
+export const NoVideosNote = styled.p`
+  font-family: Roboto;
   font-size: 18px;
   color: ${props => props.noteColor};
-  text-align: center;
 `
+
 export const RetryButton = styled.button`
   border: none;
   background-color: #4f46e5;
@@ -36,4 +37,14 @@ export const RetryButton = styled.button`
   padding-bottom: 5px;
   font-family: Roboto;
   font-size: 15px;
+`
+export const VideoCardList = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+  @media screen and (min-width: 768px) {
+    margin-left: 20px;
+  }
 `
